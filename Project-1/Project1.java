508191a2c1f95e95bb9044739b5a4a7834fee1de
Leonardo Larsen <leo@Leonardos-MacBook-Pro.local>
2023-08-21 21:04:55 -0500

    added complete code for project 1

---
+++ Project-1/Project1.java
@@ -0,0 +1,10 @@
+//basic hello world
+/* @Author: Niels Leonardo Larsen
+ * @Version: 1.0
+ * @Date: 8/21/2023
+  */
+class Project1 {
+    public static void main(String[] args) {
+        System.out.println("What do you call 8 rabbits? A rabbyte!"); 
+    }
+}
